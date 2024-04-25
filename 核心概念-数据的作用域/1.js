c0b27ae1c996748dd02ab42e4d1ef367add3d5bb
cainiaodjm <djmfrontend@163.com>
2024-04-26 02:38:12 +0800

    数据的作用域完成面试题

---
+++ 核心概念-数据的作用域/1.js
@@ -0,0 +1,10 @@
+var a = 1;
+function fn() {
+  var d = 4;
+}
+var fn1 = function () {
+  console.log("fn1");
+};
+
+fn1();
+// 在哪里定义 就是哪个作用域
---
+++ 核心概念-数据的作用域/面试题/1.js
@@ -0,0 +1,8 @@
+// 下面的代码输出什么
+console.log(a, b, c); // undedfined undefined Fn
+var a = 1;
+
+var b = function () {
+  //
+};
+function c() {}
