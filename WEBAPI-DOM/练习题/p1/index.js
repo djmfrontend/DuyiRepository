6201f76ae50615b7f05712a33db953a58a3d6d9c
djmfrontend <djmfrontend@163.com>
2024-05-24 16:18:17 +0800

    update

--- WEBAPI-DOM/练习题/p1/index.js
+++ WEBAPI-DOM/练习题/p1/index.js
@@ -1,1 +1,21 @@
 // 根据hero.js提供的数据heros，创建合适的元素，将英雄数据显示到页面上
+const container = document.querySelector(".container");
+
+for (let hero of heros) {
+  const aDom = document.createElement("a");
+  //   aDom.innerHTML = hero.cname;
+
+  const href = `https://pvp.qq.com/web201605/herodetail/${hero.ename}.shtml`;
+  aDom.href = href;
+  aDom.className = "item";
+  aDom.target = "_blank";
+  const imgDom = document.createElement("img");
+  const imgHref = `https://game.gtimg.cn/images/yxzj/img201606/heroimg/${hero.ename}/${hero.ename}.jpg`;
+  imgDom.src = imgHref;
+  imgDom.alt = "";
+  aDom.appendChild(imgDom);
+  const spanDom = document.createElement("span");
+  spanDom.innerHTML = hero.cname;
+  aDom.appendChild(spanDom);
+  container.appendChild(aDom);
+}
--- WEBAPI-DOM/练习题/p2/js/index.js
+++ WEBAPI-DOM/练习题/p2/js/index.js
@@ -1,3 +1,21 @@
 // 每隔一段时间，切换英雄的图片，让英雄动起来
 
 // 每隔一段时间，改变英雄的位置，让英雄向右移动
+
+const imgDom = document.querySelector("img");
+let i = 1;
+let step = 2;
+setInterval(() => {
+  i++;
+
+  if (i === 5) {
+    i = 1;
+  }
+  console.log(i);
+  imgDom.src = `./img/${i}.png`;
+}, 1000);
+
+setInterval(() => {
+  step += 2;
+  imgDom.style.left = `${step}px`;
+}, 200);
