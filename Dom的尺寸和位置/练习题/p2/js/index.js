4b49634085c72b09db345af42221691ade04c99c
djmfrontend <djmfrontend@163.com>
2024-06-04 20:12:36 +0800

    update

--- Dom的尺寸和位置/练习题/p2/js/index.js
+++ Dom的尺寸和位置/练习题/p2/js/index.js
@@ -1,21 +1,22 @@
 // 让小球向右下运动，遇到边缘后反弹
 const ball = document.querySelector(".ball");
-const react = ball.getBoundingClientRect();
 
-let v = 30;
-const initX = Math.floor(react.left);
-const initY = Math.floor(react.top);
-let dx = initX;
-let dy = initY;
+let v = 1;
+
 setInterval(() => {
+  const react = ball.getBoundingClientRect();
+  const initX = Math.floor(react.left);
+  const initY = Math.floor(react.top);
+  let dx = initX;
+  let dy = initY;
   dx += v;
   dy += v;
-
+  console.log(dx);
   //   console.log(ball);
   //   console.log(ball);
-  //   ball.style.transform = `translate(${dx}px,${dy}px)`;
+  ball.style.transform = `translate(${dx}px,${dy}px)`;
   //   ball.style.top = `${dy}px`;
-  //   if (dx > document.documentElement.clientWidth - react.width) {
-  //     v = -v;
-  //   }
-}, 300);
+  if (dx > document.documentElement.clientWidth - react.width) {
+    v = -v;
+  }
+}, 20);
