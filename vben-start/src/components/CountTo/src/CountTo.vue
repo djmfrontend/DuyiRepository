4c5a557726b127e895a27bb12edc34175a323c58
djmfrontend <djmfrontend@163.com>
2024-06-07 16:43:16 +0800

    update

---
+++ vben-start/src/components/CountTo/src/CountTo.vue
@@ -0,0 +1,26 @@
+<template>
+  <span> Count to </span>
+</template>
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+
+defineOptions({
+  name: "CountTo",
+});
+
+const props = defineProps({
+  startValue: {
+    type: Number,
+    default: 0,
+  },
+  endValue: {
+    type: Number,
+    default: 2024,
+  },
+});
+
+const source = ref(props.startValue);
+onMounted(() => {
+  console.log(props.startValue);
+});
+</script>
